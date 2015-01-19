1103f11058420a37da3fbb62fea538b1584e05fc
trvslhlt <trvslhlt@gmail.com>
2015-01-18 22:09:24 -0500

    adds ios app

---
+++ TextDig/TextDig/ViewControllers/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  TextDig
+//
+//  Created by trvslhlt on 1/18/15.
+//  Copyright (c) 2015 travis holt. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+  
+  override init() {
+    super.init(nibName: "ChatVC", bundle: nil)
+  }
+
+  required init(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
